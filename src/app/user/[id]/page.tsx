3b5d8634e140a8f8d9180dac9de4b781860eb0cf
bvnnal <bamapinya2503@gmail.com>
2025-03-20 18:10:53 +0700

    add best seller menus for customer

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -14,6 +14,8 @@ import { useGetTable } from "@/api/user/useTable";
 import { BaseTableResponse } from "@/interfaces/table";
 import { entryTime, remainingTime } from "@/lib/formatDate";
 import { useGetBestMenuSellers } from "@/api/general/useBestSeller";
+import Image from "next/image";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 type Props = {
   params: { id: string }
@@ -21,7 +23,8 @@ type Props = {
 
 export default function Home({ params }: Props) {
   const [search, setSearch] = useState<string>('');
-  const { setAccessCode } = useCart();
+  //const [activeTab, setActiveTab] = useState<string>("ขายดี");
+  const { setAccessCode, cart, addItem } = useCart();
   const { data: menus, isLoading: isMenuLoading } = useGetMenus(params.id);
   const { data: bestMenus, isLoading: isBestMenuLoading } = useGetBestMenuSellers();
   const { data: categories, isLoading: isLoadingCategories } = useGetCategories(params.id) as { data: BaseCategoryResponse[], isLoading: boolean };
@@ -55,20 +58,52 @@ export default function Home({ params }: Props) {
       <HeaderTabs categories={Object.keys(menusByCategory)} search={search} setSearch={setSearch} />
       <div className="flex flex-col gap-2 px-3 pt-16 pb-24">
         <div className="flex flex-row justify-between w-full">
-          <p className=" w-1/3 font-bold text-lg pl-1"> โต๊ะที่ : {table.tableName} </p>
-          <p className=" w-2/3 font-bold text-lg text-end"> เวลาในการทาน : {remainingTime(table.entryAt.toString())} นาที </p>
+          <p className=" w-1/3 font-bold text-2xl pl-1"> โต๊ะที่ : {table.tableName} </p>
+          <p className=" w-2/3 font-bold text-2xl text-end"> เวลาในการทาน : {remainingTime(table.entryAt.toString())} นาที </p>
         </div>
         <p className="text-primary text-xl text-right pr-1"> {entryTime(table.entryAt.toString())} น. </p>
         <div className="m-2 space-y-10">
 
           
-          {/* เปลี่ยนตรงนี้เป็น CARD แบบใน FIGMA */}
-          {bestMenus &&
-            <MenuList key="ขายดี" title="ขายดี" menuList={bestMenus!} />
-          }
-          {/* ----------------------------- */}
-
-
+        {bestMenus && bestMenus.length > 0 && (
+            <div id="ขายดี">
+              <p className="text-2xl font-bold mb-2">ขายดี</p>
+              <div className="grid grid-cols-1 gap-4">
+                {bestMenus.slice(0, 5).map((menu, index) => {
+                  const amount = cart.find((j) => j.menu_id === menu.id)?.quantity ?? 0;
+                  return (
+                    <div key={menu.id} className="flex border bg-white rounded-2xl">
+                      <div className="flex w-2/5 p-3 h-[125px] relative">
+                        <div className="relative w-full h-full overflow-hidden rounded-xl">
+                          <Image className='' src={menu.imageUrl} alt={menu.name} layout="fill" />
+                          <span className="absolute top-1 left-1 bg-red-500 text-white text-xs px-2 py-1 rounded-full">
+                            Popular #{index + 1}
+                          </span>
+                        </div>
+                      </div>
+                      <div className="flex flex-col w-3/5 p-4">
+                        <div className="flex flex-col h-full" >
+                          <p className="text-xl mt-[-6px] m-[-18px]">{menu.name}</p>
+                        </div>
+                        <div className="flex justify-end h-full items-end pr-3 pb-1" >
+                          <div className="flex flex-row items-center gap-3" >
+                            <div className={`hover:cursor-pointer ${amount === 0 ? "opacity-50 cursor-not-allowed" : ""}`} 
+                              onClick={() => amount > 0 && addItem({ menu_id: menu.id, quantity: -1 })}>
+                            <Icon icon="simple-line-icons:minus" fontSize={40} color={amount !== 0 ? "#FF8B13" : "#bfbfbf"} />
+                            </div>
+                            <p className='text-2xl'>{amount}</p>
+                            <div className='hover:cursor-pointer' onClick={() => addItem({ menu_id: menu.id, quantity: 1 })}>
+                              <Icon icon="simple-line-icons:plus" fontSize={40} color='#FF8B13' />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
 
           {Object.keys(menusByCategory).map((key) => {
             return <MenuList key={key} title={key} menuList={menusByCategory[key]} />;
